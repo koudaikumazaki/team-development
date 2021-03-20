8f36c58f5f3494c1b3de1dac8e5252d3bde2f360
koudaikumazaki <kk1974340@gmail.com>
2021-03-20 21:43:38 +0900

    add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :posts,  dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+  has_many :comments
 end
