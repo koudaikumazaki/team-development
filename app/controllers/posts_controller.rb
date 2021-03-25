366e03dafc042b6dc1180c27bd7f67cb2d242268
koudaikumazaki <kk1974340@gmail.com>
2021-03-25 20:34:04 +0900

    投稿内容を変更することができる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :permit_update_delete, only: [:edit, :update, :destroy]
+  # before_action :permit_update_delete, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -23,9 +23,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :new
+    end
+  end
+
   private
   def post_params
-    params.require(:post).permit(:content, :created_at, :user_id)
+    params.require(:post).permit(:content, :created_at)
   end
 
 end
