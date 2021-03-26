216b3d51293fcdc778bfb62aeadd90a96c7f1d88
house-lovers7 <taisuke.gt@gmail.com>
2021-03-26 19:55:36 +0900

    update sewsan advise

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-
-  resources :posts do
-  resources :comments          
+  
+  resources :posts do 
  end
 end
