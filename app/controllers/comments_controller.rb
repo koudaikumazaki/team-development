4a9b2f24cb8e2bd6e58fe544cb9ccbfd4e81b2a0
ryota.s.0810 <akaashi0810@gmail.com>
2021-03-26 21:39:38 +0900

    指摘事項の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,20 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   
   def create
-    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
-    @comment.post_id = @post.id
     if @comment.save
-      redirect_to post_path(@post)
+      redirect_to post_path(@comment.post)
     else
       flash[:error] = "コメントを(140文字以内で)入力してください。"
-      redirect_back(fallback_location: post_path(@post)) 
+      @post = Post.find(params[:post_id])
+      @comments = @post.comments.includes([:user])
+      render 'posts/show'
     end
   end
   
   private
   
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment).merge(params.permit(:post_id))
   end  
 end
