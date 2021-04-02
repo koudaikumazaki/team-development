bb263211e8a9c64c10283521ede5829e5d1b371d
house-lovers7 <taisuke.gt@gmail.com>
2021-04-02 22:40:19 +0900

    Userモデル：メールアドレスに全角文字が含まれていないこと

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
@@ -12,7 +14,9 @@ class User < ApplicationRecord
   end
 
   validates :encrypted_password, length: { in: 8..32 }
-  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
 end
 
 
