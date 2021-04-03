b0ad3da62e546f4b8e1622dda4a4cad13f1ac609
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 10:47:40 +0900

    Userモデルのパスワード設定の変更:半角英数字をそれぞれ1種類以上含む8文字以上32文字以下の正規表現へ

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,11 @@ class User < ApplicationRecord
   end
 
   validates :encrypted_password, length: { in: 8..32 }
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+ 
+
+
  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 describe User do
-  describe '#create' do    
-  
+  describe '#create' do
+    
+    # it "全ての項目の入力が存在すれば登録できること" do # テストしたいことの内容
+    #   user = build(:user)  # 変数userにbuildメソッドを使用して、factory_botのダミーデータを代入
+    #   user.encrypted_password = "abcdefjhasdfghjkzaadadadadada"
+    #   expect(user).to be_valid # 変数userの情報で登録がされるかどうかのテストを実行
+    # end
+
+
+    # it "passwordが存在してもencrypted_passwordがない場合は登録できないこと" do
+    #   user = build(:user, encrypted_password: "") 
+    #   user.valid?
+    #   expect(user.errors[:encrypted_password]).to include("は8文字以上で入力してください")
+    # end
+
     it "passwordが8文字以上であれば登録できること" do
       user = build(:user, encrypted_password: "12345678")
       expect(user).to be_valid
@@ -20,3 +33,23 @@ describe User do
 
   end
 end
+
+
+# create_table "users", force: :cascade do |t|
+#   t.string "email", default: "", null: false
+#   t.string "encrypted_password", default: "", null: false
+#   t.string "name", null: false
+#   t.string "reset_password_token"
+#   t.datetime "reset_password_sent_at"
+#   t.datetime "remember_created_at"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["email"], name: "index_users_on_email", unique: true
+#   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+# end
+
+
+# Userモデル
+# メールアドレスに全角文字が含まれていないこと
+
+# パスワードは半角英字と半角数字のいずれとも含まれていること
