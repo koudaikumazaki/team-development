7ef06f741eabc4026835645fccad7d096d26eab6
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 10:14:11 +0900

    Uerモデルのパスワードは8文字以上32文字以下であることのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 describe User do
-  describe '#create' do
-    it "全ての項目の入力が存在すれば登録できること" do # テストしたいことの内容
-      user = build(:user)  # 変数userにbuildメソッドを使用して、factory_botのダミーデータを代入
-      expect(user).to be_valid # 変数userの情報で登録がされるかどうかのテストを実行
+  describe '#create' do    
+  
+    it "passwordが8文字以上であれば登録できること" do
+      user = build(:user, encrypted_password: "12345678")
+      expect(user).to be_valid
     end
+
+    it "passwordが7文字以下であれば登録できないこと" do
+      user = build(:user, encrypted_password: "1234567")
+      expect(user).to be_valid
+    end
+
+    it "passwordが33文字以上であれば登録できないこと" do
+      user = build(:user, encrypted_password: "123456712345671234567123456712345")
+      expect(user).to be_valid
+    end
+
   end
 end
