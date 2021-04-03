5497689cdf6c2cfd984a9eb50f79fe7f55b6c0e7
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-03 17:32:20 +0900

    パンくずリスト「投稿編集画面」の作成

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,8 @@ crumb :show_post do |post|
   link "#{post.user.name}さんの投稿(#{post.id})"
   parent :root
 end
+
+crumb :edit_post do |post|
+  link "#{post.user.name}さんの投稿(#{post.id})の編集"
+  parent :root
+end
