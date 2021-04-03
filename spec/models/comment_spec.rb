8d450d36e996a15d6198875bd26102072a259421
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 18:04:54 +0900

    壮さんのアドバイスをテストの宣言に反映し、Emailの一意性のテストも追加した

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 describe  Comment do
   describe '#create' do
 
-      it "全ての項目の入力が存在すれば登録できること" do        
+      it "全ての項目の入力が存在すれば、コメントが作成できること" do        
         comment = build(:comment)
         expect(comment).to be_valid
       end
 
-      it "投稿がない場合は登録できないこと" do    
+      it "投稿がない場合は、コメントが作成できないこと" do    
         comment = build(:comment, comment: "")
         expect(comment).to_not be_valid         
       end
 
-      it "投稿内容が140文字以内の時は登録できること" do    
+      it "投稿内容が140文字以内の時は、コメントが作成できること" do    
         comment = build(:comment, comment: "a" * 140 )
         expect(comment).to be_valid        
       end
       
-      it "投稿内容が141文字以上の時はエラーがでること" do    
+      it "投稿内容が141文字以上の時は、コメントが作成できないこと" do    
         comment = build(:comment, comment: "a" * 141 )
         expect(comment).to_not be_valid         
       end  
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 describe Post do
   describe '#create' do
 
-    it "全ての項目の入力が存在すれば登録できること" do        
+    it "全ての項目の入力が存在すれば、投稿が作成できること" do        
       post = build(:post)
       expect(post).to be_valid
     end
 
-    it "投稿がない場合は登録できないこと" do    
+    it "投稿がない場合は、投稿が作成できないこと" do    
       post = build(:post, content: "")
       expect(post).to_not be_valid         
     end
 
-    it "投稿内容が140文字以内の時は登録できること" do    
+    it "投稿内容が140文字以内の時は、投稿が作成できること" do    
       post = build(:post, content: "a" * 140 )
       expect(post).to be_valid        
     end
 
-    it "投稿内容が141文字以上の時はエラーがでること" do    
+    it "投稿内容が141文字以上の時は、投稿が作成できないこと" do    
       post = build(:post, content: "a" * 141 )
       expect(post).to_not be_valid        
     end 
