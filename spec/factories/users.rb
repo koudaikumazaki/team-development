ce0e26914daff1ad53d02624bf91f32e332e7aff
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 12:12:48 +0900

    users.rbのパスワードのデータ設定の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
     factory :user, aliases: [:post] do
       name                  {"sample"}
       email                 {"sample@gmail.com"}
-      password              {"1234567"}
-      encrypted_password    {"1234567"}  
+      password              {"1234567a"}
+      encrypted_password    {"1234567a"}  
       created_at            { Time.zone.now }
       updated_at            { Time.zone.now }
     end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Post do
   describe '#create' do
 
-  it "全ての項目の入力が存在すれば登録できること" do
+  it "全ての項目の入力が存在すれば登録できること" do      
     post = build(:post)
     expect(post).to be_valid
   end
