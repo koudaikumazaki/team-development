1f662ea787214f148118e892483e369653309eb8
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 16:06:48 +0900

    Userモデルのテスト：encrypted_passwordと誤解して箇所をpasswordに全て入れ替えた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
     factory :user  do
       name                  {"sample"}
       email                 {"sample@gmail.com"}
-      password              {"1234567a"}
-      encrypted_password    {"1234567a"}  
+      password              {"1234567a"}        
       created_at            { Time.zone.now }
       updated_at            { Time.zone.now }
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,38 +5,38 @@ describe User do
   
 
     it "passwordが7文字以下であれば登録できないこと" do
-      user = build(:user, encrypted_password: "1234567")
+      user = build(:user, password: "1234567")
       expect(user).to be_valid
     end
 
     it "passwordが8文字以上で、半角英字と半角数字が含まれていれば登録できること" do
-      user = build(:user, encrypted_password: "1234567a")
+      user = build(:user, password: "1234567a")
       expect(user).to be_valid
     end
 
     it "passwordが8文字以上でも、半角英字が含まれていなと登録できないこと" do
-      user = build(:user, encrypted_password: "12345678")
+      user = build(:user, password: "12345678")
       expect(user).to be_valid
     end
 
     it "passwordが8文字以上でも、半角数字がが含まれていなと登録できないこと" do
-      user = build(:user, encrypted_password: "a"*8)
+      user = build(:user, password: "a"*8)
       expect(user).to be_valid
     end
   
     it "passwordが33文字以上であれば登録できないこと" do
-      user = build(:user, encrypted_password: "123456712345671234567123456712345")
+      user = build(:user, password: "123456712345671234567123456712345")
       expect(user).to be_valid
     end
 
 
     it "メールアドレスに全角文字が含まれているとテストがパスしないこと" do
-      user = build(:user, email: "さample@gmail.com", encrypted_password: "1234567a" )      
+      user = build(:user, email: "さample@gmail.com", password: "1234567a" )      
       expect(user).to be_valid
     end
 
     it "メールアドレスに全角文字が含まれていなければテストがパスすること" do
-      user = build(:user, email: "Sample@gmail.com", encrypted_password: "1234567a" )      
+      user = build(:user, email: "Sample@gmail.com", password: "1234567a" )      
       expect(user).to be_valid
     end
   end
