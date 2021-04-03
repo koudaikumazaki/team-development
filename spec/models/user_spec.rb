0a43c98792482a1265afd5bcd344fdf6b1746f6f
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 11:01:04 +0900

    Userモデルのテスト:メールアドレスに全角文字が含まれていないこと

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,12 @@ describe User do
     #   expect(user.errors[:encrypted_password]).to include("は8文字以上で入力してください")
     # end
 
-    it "passwordが8文字以上であれば登録できること" do
+    it "passwordが8文字以上で、半角英字と半角数字が含まれていれば登録できること" do
+      user = build(:user, encrypted_password: "1234567a")
+      expect(user).to be_valid
+    end
+
+    it "passwordが8文字以上でも、半角英字が含まれていなと登録できないこと" do
       user = build(:user, encrypted_password: "12345678")
       expect(user).to be_valid
     end
@@ -28,6 +33,17 @@ describe User do
 
     it "passwordが33文字以上であれば登録できないこと" do
       user = build(:user, encrypted_password: "123456712345671234567123456712345")
+      expect(user).to be_valid
+    end
+
+
+    it "メールアドレスに全角文字が含まれているとテストがパスしないこと" do
+      user = build(:user, email: "さample@gmail.com", encrypted_password: "1234567a" )      
+      expect(user).to be_valid
+    end
+
+    it "メールアドレスに全角文字が含まれていなければテストがパスすること" do
+      user = build(:user, email: "Sample@gmail.com", encrypted_password: "1234567a" )      
       expect(user).to be_valid
     end
 
